9d6d4ada64f982a42bac67e98668cd24a8503ab6
DeeBest <simphiwedladla8@gmail.com>
2024-05-04 15:44:54 +0200

    created animation for the dice and added event listener to the button to roll the dice

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const rollDiceBtn = document.getElementById('roll-dice-btn');
+const dice = document.getElementById('dice');
+
+rollDiceBtn.addEventListener('click', () => {
+  dice.classList.add('roll-animation');
+  setTimeout(() => {
+    dice.classList.remove('roll-animation');
+  }, 100);
+});
